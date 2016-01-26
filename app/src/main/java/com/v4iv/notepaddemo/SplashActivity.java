1186cf36f97a8a607d470ce0cd9aa70fcb747f9e
Vaibhav Sharma <vaibhav_sharma@outlook.in>
2016-01-27 00:59:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/v4iv/notepaddemo/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.v4iv.notepaddemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent mainActivityIntent = new Intent("com.v4iv.notepaddemo.MAINACTIVITY");
+                    startActivity(mainActivityIntent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
