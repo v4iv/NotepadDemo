0e364c1c2fe88953f459a7866fd4288c6acb0b5d
Vaibhav Sharma <vaibhav_sharma@outlook.in>
2016-01-27 01:42:26 +0530

    Second Commit

--- app/src/main/java/com/v4iv/notepaddemo/MainActivity.java
+++ app/src/main/java/com/v4iv/notepaddemo/MainActivity.java
@@ -1,13 +1,68 @@
 package com.v4iv.notepaddemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mSaveButton, mDisplayButton;
+    EditText mEditText;
+    TextView mTextView;
+
+    String data;
+    private String file = "mynote";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.editText);
+        mSaveButton = (Button) findViewById(R.id.bSave);
+        mDisplayButton = (Button) findViewById(R.id.bDisplay);
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        //Save Button
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = mEditText.getText().toString();
+                try {
+                    FileOutputStream fileOutputStream = openFileOutput(file, MODE_WORLD_READABLE);
+                    fileOutputStream.write(data.getBytes());
+                    fileOutputStream.close();
+                    Toast.makeText(getBaseContext(), "Note Saved", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //Display Button
+        mDisplayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileInputStream fileInputStream = openFileInput(file);
+                    int cursor;
+                    String temp = "";
+                    while ((cursor = fileInputStream.read()) != -1){
+                        temp = temp + Character.toString((char)cursor);
+                    }
+                    mTextView.setText(temp);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/v4iv/notepaddemo/SplashActivity.java
+++ app/src/main/java/com/v4iv/notepaddemo/SplashActivity.java
@@ -11,8 +11,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         Thread timer = new Thread() {
             public void run() {
